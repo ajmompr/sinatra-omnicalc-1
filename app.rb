b241e30ac0014ec48253b2b5245488b249555d7a
Alexandra Mompremier <58926246+ajmompr@users.noreply.github.com>
2023-09-12 16:25:24 +0000

    created /howdy route

--- app.rb
+++ app.rb
@@ -1,9 +1,27 @@
 require "sinatra"
 require "sinatra/reloader"
 
+get("/howdy") do
+  "Hello, there! Howdy!"
+  erb(:hello)
+end
+
 get("/") do
   "
   <h1>Welcome to your Sinatra App!</h1>
   <p>Define some routes in app.rb</p>
   "
 end
+
+
+# get("/square") do
+# end
+
+# get("/square_root") do
+# end
+
+# get("/payment") do  
+# end
+
+# get("/random") do
+# end
